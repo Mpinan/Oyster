7464cb241529132422d30bd14aa016f8abb3715c
Student <student@Makerss-MacBook-Pro.local>
2019-08-02 15:26:32 +0100

    Step 14

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,6 +1,14 @@
 class JourneyLog
- def initialize(journey_class: )
+ def initialize(journey_class = Journey.new )
     @journey_class = journey_class
     @journeys = []
+ end
+
+ def start(station)
+   @journeys << station
+ end
+
+ def finish(station)
+   @journeys << station
  end
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,8 +1,21 @@
 require 'journey_log'
+
 describe JourneyLog do
-    let(:journey){ double {:entry_station entry_station, :exit_station exit_station}}
+    let(:journey){ double entry_station: station, exit_station: station}
     let(:station){ double :station}
     let(:journey_class){double :journey_class, new: journey}
     subject {described_class.new(journey_class: journey_class)}
 
+  describe '#start' do
+    it 'starts a journey' do
+        expect(journey_class).to receive(:new).with(entry_station: station)
+        subject.start(station)
+    end
+    
+    it 'records a journey' do
+        allow(journey_class).to receive(:new).and_return journey
+        subject.start(station)
+        expect(subject.journey).to include journey
+    end
+  end
 end
